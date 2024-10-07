250d770a8f86a68b69492cf5e44e1d154c886b6f
Kshitij Shresth <kshitijshresth@icloud.com>
2024-10-07 07:02:52 +0000

    Update and rename 1281. Subtract the Product and Sum of Digits of an Integer to 1281. Subtract the Product and Sum of Digits of an Integer.java

--- 1281. Subtract the Product and Sum of Digits of an Integer.java
+++ 1281. Subtract the Product and Sum of Digits of an Integer.java
@@ -2,7 +2,7 @@ class Solution {
     public int subtractProductAndSum(int n) {
         int product = 1;
         int sum = 0;
-        
+//Initialize the product and sum variables        
              
         while (n > 0) {
             int digit = n % 10;  
@@ -10,6 +10,7 @@ class Solution {
             sum += digit;       
             n /= 10;        
         }
+//Loop repeats with the new n value 1 digit short   
         
         return product - sum;  
     }
